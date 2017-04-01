16d5b64ed7bcd8b0bc4a96ced31eab8b2fb8838f
Phillip Scalo <phillip.scalo@gmail.com>
2017-04-01 19:10:25 -0400

    printing using loops

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,3 +1,14 @@
 #!/usr/bin/env python3
 
+"""FizzBuzz programming exercise"""
 
+
+for i in range(101):
+    if i%3==0 and i%5==0:
+        print("FizzBuzz")
+    if i%3==0:
+        print("Fizz")
+    if i%5==0:
+        print("Buzz")
+    else:
+        print(i)
