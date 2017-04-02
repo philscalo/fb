a871a2cfdf3433a2390106064767a26730d9dd81
Phillip Scalo <phillip.scalo@gmail.com>
2017-04-01 21:09:37 -0400

    adding back

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -3,12 +3,15 @@
 """FizzBuzz programming exercise"""
 
 
-for i in range(101):
-    if i%3==0 and i%5==0:
-        print("FizzBuzz")
-    if i%3==0:
-        print("Fizz")
-    if i%5==0:
-        print("Buzz")
-    else:
-        print(i)
+def fizz_buzz_assess():
+    for i in range(101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        if i % 3 == 0:
+            print("Fizz")
+        if i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+print(__name__)
